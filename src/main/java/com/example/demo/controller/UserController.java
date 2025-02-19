b5caaef264f8d0125b11a3a3fb373a60193777d6
Rick Li <racke1983cn@hotmail.com>
2025-02-19 09:25:12 -0500

    fix: permission

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.config.RedisConfig;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
@@ -28,10 +25,10 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+    // @GetMapping
+    // public List<User> getAllUsers() {
+    //     return userRepository.findAll();
+    // }
 
     @GetMapping("/test")
     @ResponseBody
