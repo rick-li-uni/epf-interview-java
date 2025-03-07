d007fcdfd61848f6bd0d6d138eb81bc6137f5bd3
Rick Li <racke1983cn@hotmail.com>
2025-03-07 08:48:58 -0500

    refactor: ++ logs.

--- src/main/java/com/example/demo/controller/FileUploadController.java
+++ src/main/java/com/example/demo/controller/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.FileUploadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,12 +10,15 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/file/upload")
 public class FileUploadController {
+    Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
     @Autowired
     private FileUploadService fileUploadService;
 
     @PostMapping("/product/{productId}")
     public void upload(@RequestBody MultipartFile file, @PathVariable Long productId){
+        logger.info("uploading file {}", file.getOriginalFilename());
+
         fileUploadService.upload(file, productId);
     }
 
